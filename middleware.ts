2f2a9b6013d9d06186132867a1be5a0a512110ff
Martinchoe <martindoval@gmail.com>
2024-11-23 15:21:56 -0300

    Middleware Fixed

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+import { auth } from "./auth";
+//import { getToken } from "next-auth/jwt";
 
 const paginasAdministrativas = ["/admin"]
 
 export async function middleware(req: NextRequest) {
-  const session = await getToken({req, secret: process.env.AUTH_SECRET})  
+  //const session = await getToken({req, secret: process.env.AUTH_SECRET})  
+  
+  const session = await auth();
   console.log(session)
 
   if(!session) {
     return NextResponse.redirect(new URL("/login", req.url))
   }  
-  if(paginasAdministrativas.includes(req.nextUrl.pathname) && session.role !== "admin") {
+  if(paginasAdministrativas.includes(req.nextUrl.pathname) && session.user.role !== "admin") {
     return NextResponse.redirect(new URL("/error", req.url))
   }
   return NextResponse.next()
@@ -19,10 +22,3 @@ export async function middleware(req: NextRequest) {
 export const config = {
   matcher: ['/admin', '/profile', '/dashboard']
 };
-
-/*
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)","/admin"],
-};
-*/
-
